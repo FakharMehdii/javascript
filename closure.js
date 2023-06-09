6fed2b8473fb0fa39e80758a51e2f664314b88e3
Fakhar Mehdi <fakhar.mehdi@mergestack.com>
2023-06-09 18:07:54 +0500

    🎉 Studied ES5, ES6,jquery, loadash

---
+++ closure.js
@@ -0,0 +1,21 @@
+function myFunc() {
+    let day = 1;
+  
+    function incrementDay() {
+      if (day === 7) {
+        day = 1;
+      } else {
+        day++;
+      }
+  
+      console.log(day);
+    }
+  
+    return incrementDay;
+  }
+  
+  const myVar = myFunc();
+  myVar();
+/
+  🎉
+  
